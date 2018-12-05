348a5deb4eef012791168d3598052d0eef30708a
王舵 <wd14931@ly.com>
2018-12-06 01:01:14 +0800

    到选座环节

--- controllers/index.js
+++ controllers/index.js
@@ -145,15 +145,104 @@ class Index{
    */
   remainseat(req, res, next){
     let id   = req.query.id;
-    let date = req.query.date;
 
-    let url = 'http://ticket.yes24.com/Pages/Perf/Detail/Ajax/PerfPlayTime.aspx';
+    let url = 'http://ticket.yes24.com/Pages/Perf/Detail/Ajax/PerfRemainSeat.aspx';
     let param = {
-      IdPerf: id,//30851 （演唱会的id）
-      PlayDate: date//20181227
+      IdTime: id,//30851 （演唱会的id）
+      IdLock: 0//20181227
     };
 
     request.post({url:url, form: param}, function (error, response, body) {
+      console.log('ssssssssss', body)
+
+      if(error){
+        return next(error);
+      }
+
+      if (response.statusCode == 200) {
+        res.send(body)
+      }
+    })
+  }
+
+  /**
+   * 获取演唱会详情
+   * @param req
+   * @param res
+   * @param next
+   * 获取到这里，就是打开iframe弹框了
+     调用第一个接口;
+     Request URL: http://ticket.yes24.com/Pages/Perf/Sale/Ajax/Perf/PerfTime.aspx
+     Request Method: POST
+     Status Code: 200 OK
+     参数
+     pDay: 20181222
+     pIdPerf: 30851
+     pIdCode:
+     pIsMania: 0 写
+   */
+  fetchplayinfo(req, res, next){
+    let id   = req.query.id;
+    let date = req.query.date;
+
+    /**
+     * 这个接口依赖于一个cookie
+     * Cookie:ServiceCookies=MTQ0NDQzMjRgZmVpc2U1MTQ3YFpIT1UgWUlOR2AwYD
+     *    A1YHNoaWJhYGAxNDQ0NDMyNGAzMWBNTS9sU21DOVlKVXZQbnBpNmRJa2hzSkZtY3RpU2xBQk9MaER3dWNtcVpNZUN1aGk3NS9WNjhVRHF4
+     *    ZG43Y291eW5jRTJLQlFDaEQxL2VyVlRFcEVVZz09YGNoL3
+     *    R2U3U5d1JYZ0dMQy85aiszSWZKcHJGWll1L2NOSVJ6TnlsTmFNS3
+     *    RRVkRlTnNBSmRwcTdFZFBkZ2JIRWxjYUl0Qm5NOHI4SGUwZW1rMjhGaUE2N3BuNktqQVg0UXp6Nmtvd0hScjBwTGJNVmha
+     * MG5Qa1lRYm9UcW1tdWw3c2Fabm9XK2UrcmtjdGUyQ1pKeWROc0J2MWVQVVR2TlkrbSt0NUVmV2JZTT1gNDJpSjRwbkRPbE1iVkthZko1NG5SZz09;
+     * @type {string}
+     * 这个里面会返回比较13:00，第一集，15:00,第二集，然后用户选择了这两个里面的任意一个，然后点击按钮，获取选座位的一页面
+     * 这个下面会调用下面的接口
+     */
+    let url = 'http://ticket.yes24.com/Pages/Perf/Sale/Ajax/Perf/PerfTime.aspx';
+    let param = {
+      pDay: date,//'20181221',
+      pIdPerf: id,//'30851',
+      pIdCode:'',
+      pIsMania: '0',
+    };
+    // todo 必须要有cookie
+    request.post({url:url, form: param, headers: {
+        'Cookie': 'ServiceCookies=MTQ0NDQzMjRgZmVpc2U1MTQ3YFpIT1UgWUlOR2AwYDA1YHNoaWJhYGAxNDQ0NDMyNGAzMWBNTS9sU21DOVlKVXZQbnBpNmRJa2hzSkZtY3RpU2xBQk9MaER3dWNtcVpNZUN1aGk3NS9WNjhVRHF4ZG43Y291eW5jRTJLQlFDaEQxL2VyVlRFcEVVZz09YGNoL3R2U3U5d1JYZ0dMQy85aiszSWZKcHJGWll1L2NOSVJ6TnlsTmFNS3RRVkRlTnNBSmRwcTdFZFBkZ2JIRWxjYUl0Qm5NOHI4SGUwZW1rMjhGaUE2N3BuNktqQVg0UXp6Nmtvd0hScjBwTGJNVmhaMG5Qa1lRYm9UcW1tdWw3c2Fabm9XK2UrcmtjdGUyQ1pKeWROc0J2MWVQVVR2TlkrbSt0NUVmV2JZTT1gNDJpSjRwbkRPbE1iVkthZko1NG5SZz09'
+      }}, function (error, response, body) {
+      console.log('ssssssssss111', error, body)
+
+      if(error){
+        return next(error);
+      }
+
+      if (response.statusCode == 200) {
+        res.send(body)
+      }
+    })
+  }
+
+  /**
+   * 上面获取到了当前一天的两集，然后点击一集的时候会调用这个接口，获取这一集的作为大致信息，然后用户点击下一步来选择（座位图）
+   * Request URL: http://ticket.yes24.com/Pages/Perf/Sale/Ajax/Perf/TimeSeatRemain.aspx
+     Request Method: POST
+     参数：
+     pIdTime: 976612
+     pIdCode:
+     pDisplayRemainSeat: 0
+   */
+  fetchseatprice(req, res, next){
+
+    let id   = req.query.id;
+    let url = 'http://ticket.yes24.com/Pages/Perf/Sale/Ajax/Perf/TimeSeatRemain.aspx';
+    let param = {
+      pIdTime: id,//976612,
+      pIdCode:'',
+      pDisplayRemainSeat: 0,
+    };
+
+    // todo 必须要有cookie
+    request.post({url:url, form: param, headers: {
+        'Cookie': 'ServiceCookies=MTQ0NDQzMjRgZmVpc2U1MTQ3YFpIT1UgWUlOR2AwYDA1YHNoaWJhYGAxNDQ0NDMyNGAzMWBNTS9sU21DOVlKVXZQbnBpNmRJa2hzSkZtY3RpU2xBQk9MaER3dWNtcVpNZUN1aGk3NS9WNjhVRHF4ZG43Y291eW5jRTJLQlFDaEQxL2VyVlRFcEVVZz09YGNoL3R2U3U5d1JYZ0dMQy85aiszSWZKcHJGWll1L2NOSVJ6TnlsTmFNS3RRVkRlTnNBSmRwcTdFZFBkZ2JIRWxjYUl0Qm5NOHI4SGUwZW1rMjhGaUE2N3BuNktqQVg0UXp6Nmtvd0hScjBwTGJNVmhaMG5Qa1lRYm9UcW1tdWw3c2Fabm9XK2UrcmtjdGUyQ1pKeWROc0J2MWVQVVR2TlkrbSt0NUVmV2JZTT1gNDJpSjRwbkRPbE1iVkthZko1NG5SZz09;'
+      }}, function (error, response, body) {
       console.log('ssssssssss', body)
 
       if(error){
